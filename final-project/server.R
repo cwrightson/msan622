781908f7f1c2acb0006e1a03ebcb819786f946ac
cwrightson <cole.wrightson@gmail.com>
2014-05-15 23:57:05 -0700

    plots done

--- final-project/server.R
+++ final-project/server.R
@@ -691,7 +691,7 @@ plot3 <- function(dataset, geo, when, us_int, seasons){
  p <- p + geom_area()
  
  palette <- c('#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#66c2a4', 
-              '#ffff33','#a65628','#f781bf','#999999', '#000000')
+              '#ffff33','#a65628','#f781bf','#999999')
  
  p <- p + scale_fill_manual(values = palette)
  p <- p + xlab("Season")
